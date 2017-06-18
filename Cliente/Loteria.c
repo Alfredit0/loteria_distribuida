d38ceb5d297c3b42c423c89b9ecca275ef008cd6
Alfredo Reyes <benitoalfredoreyes@gmail.com>
2017-06-18 11:20:26 -0500

    incrustando logica en interfaz

--- Cliente/Loteria.c
+++ Cliente/Loteria.c
@@ -1,6 +1,8 @@
 #include "Loteria.h"
 #include "crear.h" 
 char *filename;	
+#define MAX_LINE 10
+extern int errno;
 static GtkItemFactoryEntry menu_items[] = {
   
   { "/_Instrucciones",      NULL,         NULL, 0, "<LastBranch>" },
@@ -29,7 +31,7 @@ void get_main_menu( GtkWidget  *window,
     *menubar = gtk_item_factory_get_widget (item_factory, "<main>");
 }
 
-  GtkWidget *main_vbox;
+GtkWidget *main_vbox;
 
 int main( int argc,char *argv[] )
 {
@@ -37,7 +39,7 @@ int main( int argc,char *argv[] )
     gtk_init (&argc, &argv);
 	//Inicializamos la variable window
 	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_icon(GTK_WINDOW(window), create_pixbuf("icon.png"));//coloca el icono del programa
+	gtk_window_set_icon(GTK_WINDOW(window), create_pixbuf("Imagenes/icon.png"));//coloca el icono del programa
 	gtk_signal_connect (GTK_OBJECT (window), "destroy",GTK_SIGNAL_FUNC (gtk_main_quit), "WM destroy");
 	//Ponemos nombre en la ventana
 	gtk_window_set_title (GTK_WINDOW (window), "Loteria");
@@ -74,7 +76,7 @@ void Principal()
 	botoncerrar=gtk_button_new_with_label("salir");//salir
 	gtk_widget_set_size_request(botoncerrar,200, 70);
 	gtk_fixed_put(GTK_FIXED(contenedor),botoncerrar,710,650);	
-	imagen=gtk_image_new_from_file("inicio.jpg");	
+	imagen=gtk_image_new_from_file("Imagenes/inicio.jpg");	
 	gtk_fixed_put(GTK_FIXED(contenedor),imagen,0,0);
 	g_signal_connect_swapped (G_OBJECT (botoncerrar), "clicked",
                                   G_CALLBACK (gtk_widget_destroy),
@@ -88,99 +90,129 @@ void Principal()
 	gdk_threads_leave();		
 }
 
-void Ayuda()
-{
-	//~Declaración de variables a utilizar en esta función
-	GtkWidget *img,*reg,*clic,*manual;
-	//~Inicializamos la variable window encima de la actual
-	vent = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-	//~Inicializamos el contenedor
-	conte=gtk_fixed_new();
-	
-	
-	
-	manual =gtk_link_button_new_with_label( "manual.pdf","Manual de Usuario" );
-	gtk_link_button_set_uri_hook( link_hook, NULL, NULL );
-	
-	//~Definimos el tamaño del boton b
-	gtk_widget_set_size_request(manual,168,50);
-	//~Poscicionamos b en el contenedor conte 
-	gtk_fixed_put (GTK_FIXED(conte),manual,212,570);
-	
-	//~Dejamos la ventana de forma estática
-	gtk_window_set_resizable(GTK_WINDOW(vent),FALSE);
-	//~Ponemos nombre a la ventana
-	gtk_window_set_title (GTK_WINDOW (vent), "Ayuda");
-	//~Poscicionamos la ventana en el centro de la pantalla
-	gtk_window_set_position(GTK_WINDOW(vent), GTK_WIN_POS_CENTER);
-	//~Definimos el tamaño d ela ventana
-	gtk_widget_set_size_request (vent, 572, 650);
-	clic=gtk_image_new_from_file ("clic.gif");
-	gtk_fixed_put (GTK_FIXED(conte),clic,20,505);
-//~ Ponemos la imegen de fondo.
-	img=gtk_image_new_from_file ("ayuda.jpg");
-	gtk_fixed_put (GTK_FIXED(conte),img,0,0);
+void Jugar()
+{    
+		//LOGICA DEL CLIENTE
+		char mensaje[MAX_LINE];
+	   char buf[MAX_LINE];
+	   struct sockaddr_in fsock, sname;
+	   struct hostent *hent; /* estructura que guarda el llamado a gethostbyname */
+	   int s, len;	   
+	   char *sl="A1"; 
+	   hent = gethostbyname("localhost");
+	   //Creación de socket 
+	   if((s=socket(AF_INET,SOCK_STREAM,0)) < 0) {
+		  perror("SOCKET: ");
+		  exit(0);
+	   }
+	   fsock.sin_family = AF_INET;
+	   fsock.sin_addr.s_addr = *(long *) hent->h_addr; /* direccion IP de Maq. Remota */
+	   fsock.sin_port = htons(4400); /* puerto de la maq. remota */
+	   //Estableciendo conexión 
+	   if(connect(s,(struct sockaddr *)&fsock, sizeof(struct sockaddr_in)) == -1){
+		  perror("CONNECT: ");
+		  close(s);
+		  exit(0);
+	   }
+	   printf("Arranca el Programa Cliente !!!... Pulse q para salir\n");
+	   strcpy(mensaje,"conectame");
+	   //printf("Escribe tu mensaje de Jugador ");
+	   //scanf("%s",mensaje);
+	   //printf("Tu mensaje es = %s\n",mensaje);
+	   //Transferencia de datos 
+	   if( send(s,mensaje,strlen(mensaje),0) < strlen(mensaje) ){
+		perror("SEND: ");    
+	   }
+		//printf("Me detengo a recibir la respuesta del servidor...\n");
+		//Transferencia de datos 
+		if( (len=recv(s,buf,MAX_LINE-1,0))<= 0 ){
+			 perror("RECV: ");
+			 close(s);
+			 exit(0);
+		 }
+	   buf[len] = '\0';
+	   printf("Respuesta..: %s\n\n",buf);   
+	   if(strcmp(buf,"RECHAZADO")!=0){//
+		   while(1){
+				//Asignacion de mensaje
+			   strcpy(mensaje,"yaempiezo");  
+			   //Envio de datos 
+			   if( send(s,mensaje,strlen(mensaje),0) < strlen(mensaje) ){
+				perror("SEND: ");    
+			   }			
+				//Transferencia de datos 
+				if( (len=recv(s,buf,MAX_LINE-1,0))<= 0 ){
+					 perror("RECV: ");
+					 close(s);
+					 exit(0);
+				 }
+			   buf[len] = '\0';
+			   //printf("Respuesta..: %s\n\n",buf);
+			   if(strcmp(buf,"COMIENZAN")==0){//   			   
+				   while(1){
+						//Asignacion de mensaje
+						strcpy(mensaje,"damecarta");  
+						//Envio de datos 
+						if( send(s,mensaje,strlen(mensaje),0) < strlen(mensaje) ){
+							perror("SEND: ");    
+						}			
+						//Transferencia de datos 
+						if( (len=recv(s,buf,MAX_LINE-1,0))<= 0 ){
+							perror("RECV: ");
+							close(s);
+							exit(0);
+						}
+						buf[len] = '\0';
+						printf("Carta lanzada..: %s\n\n",buf);	
+						sleep(6);				
+						if(strcmp(buf,"YAGANARON")==0){//   
+							break;
+						}						
+					}
+				}
+			   if(strcmp(buf,"NINGUNAOP")==0){//   
+				break;
+				}		
+		   }	
+		}else{
+		printf("Jugador Rechazado!! Intente mas tarde\n\n");	
+		}	   	
+	//TERMINA LOGICA DEL CLIENTE
+  //gtk_widget_hide_all(window);
 
+        window_c = GTK_WINDOW(gtk_window_new(GTK_WINDOW_TOPLEVEL));//Crea la ventana principal
+        gtk_window_set_icon(GTK_WINDOW(window_c), create_pixbuf("Imagenes/icono.jpg"));//coloca el icono del programa
+        //Definimos el tamaño de la ventana
+	gtk_widget_set_size_request (window_c,1063, 768);
+	//Deja la ventana estática
+	gtk_window_set_resizable(GTK_WINDOW(window_c),FALSE);
+	//Posciciona la venta en el centro de la pantalla
+	gtk_window_set_position(GTK_WINDOW(window_c), GTK_WIN_POS_CENTER);
 
-	gtk_container_add (GTK_CONTAINER(vent),conte);
-	gtk_widget_show_all(vent);
-}
+        frame=gtk_fixed_new();// Se crea un marco
+        gtk_container_add(GTK_CONTAINER(window_c), frame);//se agrega el marco al contenedor
 
-void Desarrolladores()
-{
-	//~Declaración de variables a utilizar en esta función
-	GtkWidget *img,*reg,*clic,*manual;
-	//~Inicializamos la variable window encima de la actual
-	vent = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-	//~Inicializamos el contenedor
-	conte=gtk_fixed_new();
-img=gtk_image_new_from_file ("desarrolladores.jpg");
-	gtk_fixed_put (GTK_FIXED(conte),img,0,0);
-	
-//~Dejamos la ventana de forma estática
-	gtk_window_set_resizable(GTK_WINDOW(vent),FALSE);
-	//~Ponemos nombre a la ventana
-	gtk_window_set_title (GTK_WINDOW (vent), "Desarrolladores");
-	//~Poscicionamos la ventana en el centro de la pantalla
-	gtk_window_set_position(GTK_WINDOW(vent), GTK_WIN_POS_CENTER);
-	//~Definimos el tamaño d ela ventana
-	gtk_widget_set_size_request (vent, 572, 650);
-	
-	gtk_container_add (GTK_CONTAINER(vent),conte);
-	gtk_widget_show_all(vent);
-}
-
-void Instrucciones()
-{
-	//~Declaración de variables a utilizar en esta función
-	GtkWidget *img,*reg,*clic,*manual;
-	//~Inicializamos la variable window encima de la actual
-	vent = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-	//~Inicializamos el contenedor
-	conte=gtk_fixed_new();
-	//~Dejamos la ventana de forma estática
-	gtk_window_set_resizable(GTK_WINDOW(vent),FALSE);
-	//~Ponemos nombre a la ventana
-	gtk_window_set_title (GTK_WINDOW (vent), "Instrucciones");
-	//~Poscicionamos la ventana en el centro de la pantalla
-	gtk_window_set_position(GTK_WINDOW(vent), GTK_WIN_POS_CENTER);
-	//~Definimos el tamaño d ela ventana
-	gtk_widget_set_size_request (vent,572, 650);
-	
-	
-
-	img=gtk_image_new_from_file ("instrucciones.jpg");
-	gtk_fixed_put (GTK_FIXED(conte),img,0,0);
-
-	gtk_container_add (GTK_CONTAINER(vent),conte);
-	gtk_widget_show_all(vent);
-}
-
-static void link_hook( GtkLinkButton *button,const gchar   *link,gpointer       data )
-{
-   gchar *cmd = g_strconcat( "xdg-open ", link, NULL );
-
-   g_spawn_command_line_async( cmd, NULL );
-   g_free( cmd );
+	//se asigna la iimagen de fondo
+	imagen=gtk_image_new_from_file("Imagenes/tablero2.jpg");
+  	gtk_fixed_put(GTK_FIXED(frame),imagen,0,0); 
+	//Creacion del boton de cerrar
+        botoncerrar=gtk_button_new_with_label("salir");
+        gtk_widget_set_size_request(botoncerrar,180,70);
+        gtk_fixed_put (GTK_FIXED(frame),botoncerrar,840,600);
+	g_signal_connect(botoncerrar, "clicked", G_CALLBACK (destroy),NULL);	
+	//Creacion del boton de menu
+	boton1=gtk_button_new_with_label("menu");
+        gtk_widget_set_size_request(boton1,180,70);
+        gtk_fixed_put (GTK_FIXED(frame),boton1,600,600);
+	/*Creacion del boton de ayuda
+	boton2=gtk_button_new_with_label("Ayuda");
+        gtk_widget_set_size_request(boton2,180,70);
+        gtk_fixed_put (GTK_FIXED(frame),boton2,20,50);*/
+        // g_signal_connect(b1, "clicked", G_CALLBACK (Jugar),NULL);
+	g_signal_connect_swapped (G_OBJECT (boton1), "clicked",G_CALLBACK (gtk_widget_destroy),G_OBJECT (GTK_WIDGET(window_c)));
+        //g_signal_connect_swapped (G_OBJECT (boton1), "clicked",G_CALLBACK (gtk_widget_show),G_OBJECT (GTK_WIDGET(window)));
+        g_signal_connect(botoncerrar, "clicked", G_CALLBACK (destroy),NULL);
+        //Mostramos todos los elementos
+	  gtk_widget_show_all(GTK_WIDGET(window_c));
 }
 
--- Cliente/crear.c
+++ Cliente/crear.c
@@ -14,49 +14,93 @@ GdkPixbuf *create_pixbuf(const gchar * filename)
  }  
 
 
-
+void Ayuda()
+{
+	//~Declaración de variables a utilizar en esta función
+	GtkWidget *img,*reg,*clic,*manual;
+	//~Inicializamos la variable window encima de la actual
+	vent = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	//~Inicializamos el contenedor
+	conte=gtk_fixed_new();
 	
-void Jugar()
-{    
-  //gtk_widget_hide_all(window);
+	
+	
+	manual =gtk_link_button_new_with_label( "manual.pdf","Manual de Usuario" );
+	gtk_link_button_set_uri_hook( link_hook, NULL, NULL );
+	
+	//~Definimos el tamaño del boton b
+	gtk_widget_set_size_request(manual,168,50);
+	//~Poscicionamos b en el contenedor conte 
+	gtk_fixed_put (GTK_FIXED(conte),manual,212,570);
+	
+	//~Dejamos la ventana de forma estática
+	gtk_window_set_resizable(GTK_WINDOW(vent),FALSE);
+	//~Ponemos nombre a la ventana
+	gtk_window_set_title (GTK_WINDOW (vent), "Ayuda");
+	//~Poscicionamos la ventana en el centro de la pantalla
+	gtk_window_set_position(GTK_WINDOW(vent), GTK_WIN_POS_CENTER);
+	//~Definimos el tamaño d ela ventana
+	gtk_widget_set_size_request (vent, 572, 650);
+	clic=gtk_image_new_from_file ("clic.gif");
+	gtk_fixed_put (GTK_FIXED(conte),clic,20,505);
+//~ Ponemos la imegen de fondo.
+	img=gtk_image_new_from_file ("ayuda.jpg");
+	gtk_fixed_put (GTK_FIXED(conte),img,0,0);
 
-        window_c = GTK_WINDOW(gtk_window_new(GTK_WINDOW_TOPLEVEL));//Crea la ventana principal
-        gtk_window_set_icon(GTK_WINDOW(window_c), create_pixbuf("icono.jpg"));//coloca el icono del programa
-        //Definimos el tamaño de la ventana
-	gtk_widget_set_size_request (window_c,1063, 768);
-	//Deja la ventana estática
-	gtk_window_set_resizable(GTK_WINDOW(window_c),FALSE);
-	//Posciciona la venta en el centro de la pantalla
-	gtk_window_set_position(GTK_WINDOW(window_c), GTK_WIN_POS_CENTER);
 
-        frame=gtk_fixed_new();// Se crea un marco
-        gtk_container_add(GTK_CONTAINER(window_c), frame);//se agrega el marco al contenedor
-
-	//se asigna la iimagen de fondo
-	imagen=gtk_image_new_from_file("tablero2.jpg");
-  	gtk_fixed_put(GTK_FIXED(frame),imagen,0,0); 
-	//Creacion del boton de cerrar
-        botoncerrar=gtk_button_new_with_label("salir");
-        gtk_widget_set_size_request(botoncerrar,180,70);
-        gtk_fixed_put (GTK_FIXED(frame),botoncerrar,840,600);
-	g_signal_connect(botoncerrar, "clicked", G_CALLBACK (destroy),NULL);	
-	//Creacion del boton de menu
-	boton1=gtk_button_new_with_label("menu");
-        gtk_widget_set_size_request(boton1,180,70);
-        gtk_fixed_put (GTK_FIXED(frame),boton1,600,600);
-	/*Creacion del boton de ayuda
-	boton2=gtk_button_new_with_label("Ayuda");
-        gtk_widget_set_size_request(boton2,180,70);
-        gtk_fixed_put (GTK_FIXED(frame),boton2,20,50);*/
-        // g_signal_connect(b1, "clicked", G_CALLBACK (Jugar),NULL);
-	g_signal_connect_swapped (G_OBJECT (boton1), "clicked",G_CALLBACK (gtk_widget_destroy),G_OBJECT (GTK_WIDGET(window_c)));
-        //g_signal_connect_swapped (G_OBJECT (boton1), "clicked",G_CALLBACK (gtk_widget_show),G_OBJECT (GTK_WIDGET(window)));
-        g_signal_connect(botoncerrar, "clicked", G_CALLBACK (destroy),NULL);
-        //Mostramos todos los elementos
-	  gtk_widget_show_all(GTK_WIDGET(window_c));
+	gtk_container_add (GTK_CONTAINER(vent),conte);
+	gtk_widget_show_all(vent);
 }
 
+void Desarrolladores()
+{
+	//~Declaración de variables a utilizar en esta función
+	GtkWidget *img,*reg,*clic,*manual;
+	//~Inicializamos la variable window encima de la actual
+	vent = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	//~Inicializamos el contenedor
+	conte=gtk_fixed_new();
+img=gtk_image_new_from_file ("desarrolladores.jpg");
+	gtk_fixed_put (GTK_FIXED(conte),img,0,0);
+	
+//~Dejamos la ventana de forma estática
+	gtk_window_set_resizable(GTK_WINDOW(vent),FALSE);
+	//~Ponemos nombre a la ventana
+	gtk_window_set_title (GTK_WINDOW (vent), "Desarrolladores");
+	//~Poscicionamos la ventana en el centro de la pantalla
+	gtk_window_set_position(GTK_WINDOW(vent), GTK_WIN_POS_CENTER);
+	//~Definimos el tamaño d ela ventana
+	gtk_widget_set_size_request (vent, 572, 650);
+	
+	gtk_container_add (GTK_CONTAINER(vent),conte);
+	gtk_widget_show_all(vent);
+}
 
+void Instrucciones()
+{
+	//~Declaración de variables a utilizar en esta función
+	GtkWidget *img,*reg,*clic,*manual;
+	//~Inicializamos la variable window encima de la actual
+	vent = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	//~Inicializamos el contenedor
+	conte=gtk_fixed_new();
+	//~Dejamos la ventana de forma estática
+	gtk_window_set_resizable(GTK_WINDOW(vent),FALSE);
+	//~Ponemos nombre a la ventana
+	gtk_window_set_title (GTK_WINDOW (vent), "Instrucciones");
+	//~Poscicionamos la ventana en el centro de la pantalla
+	gtk_window_set_position(GTK_WINDOW(vent), GTK_WIN_POS_CENTER);
+	//~Definimos el tamaño d ela ventana
+	gtk_widget_set_size_request (vent,572, 650);
+	
+	
+
+	img=gtk_image_new_from_file ("instrucciones.jpg");
+	gtk_fixed_put (GTK_FIXED(conte),img,0,0);
+
+	gtk_container_add (GTK_CONTAINER(vent),conte);
+	gtk_widget_show_all(vent);
+}
 		
 void Reglas_Juego()
 {    
@@ -114,5 +158,14 @@ void AcercaDe()
 void destroy( GtkWidget *widget,gpointer   data )
 {
     gtk_main_quit();
+}
+
+
+static void link_hook( GtkLinkButton *button,const gchar   *link,gpointer       data )
+{
+   gchar *cmd = g_strconcat( "xdg-open ", link, NULL );
+
+   g_spawn_command_line_async( cmd, NULL );
+   g_free( cmd );
 }	
 
